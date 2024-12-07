fa5076fc6042f77f5b909fcea611206cfb023689
farid-hacibabazade <faridhaci0@gmail.com>
2024-12-07 23:12:21 +0400

    initial commit

---
+++ src/main/java/com/example/taskl12jpa/TaskL12jpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskl12jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskL12jpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskL12jpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskl12jpa/TaskL12jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskl12jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskL12jpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
